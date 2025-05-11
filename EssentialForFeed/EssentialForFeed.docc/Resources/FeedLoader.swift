258b02b44c2db34314b695d8bd6d70e9bf3d20c1
Jair Ramírez-Gastón <Jair Ramrez-Gastn NTT DATA - Jorge Ramirez nttdata-jorge.ramirez@mibanco.com.pe>
2025-05-11 10:29:52 -0500

    Create FeedLoader protocol for boundary

---
+++ EssentialForFeed/EssentialForFeed.docc/Resources/FeedLoader.swift
@@ -0,0 +1,19 @@
+//
+//  FeedLoader.swift
+//  
+//
+//  Created by Jorge Jair Ramirez Gaston Zuloeta on 11/05/25.
+//
+
+
+enum ResultOfLoadFeed {
+    case success([FeedItem])
+    case failure(Error)
+}
+
+
+protocol FeedLoader {
+    
+    func loadItems(completion: @escaping (ResultOfLoadFeed) -> Void)
+    
+}
