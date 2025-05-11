f0e047b8af406c0d7f6629bfa74f00df0fea48b3
Jair Ramírez-Gastón <Jair Ramrez-Gastn NTT DATA - Jorge Ramirez nttdata-jorge.ramirez@mibanco.com.pe>
2025-05-11 10:21:41 -0500

    create framework

---
+++ EssentialForFeed/EssentialForFeed.docc/Resources/FeedItem.swift
@@ -0,0 +1,16 @@
+//
+//  FeedItem.swift
+//  
+//
+//  Created by Jorge Jair Ramirez Gaston Zuloeta on 11/05/25.
+//
+
+import Foundation
+
+struct FeedItem {
+    
+    let id: UUID
+    let description: String?
+    let location: String?
+    let iamgeURL: URL
+}
---
+++ EssentialForFeed/EssentialForFeed.h
@@ -0,0 +1,18 @@
+//
+//  EssentialForFeed.h
+//  EssentialForFeed
+//
+//  Created by Jorge Jair Ramirez Gaston Zuloeta on 11/05/25.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for EssentialForFeed.
+FOUNDATION_EXPORT double EssentialForFeedVersionNumber;
+
+//! Project version string for EssentialForFeed.
+FOUNDATION_EXPORT const unsigned char EssentialForFeedVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EssentialForFeed/PublicHeader.h>
+
+
